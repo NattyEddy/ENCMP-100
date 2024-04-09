89cbf3d30aadf89131ac40363b68fd0c79b8dcd9
Nathan E <99678920+NattyEddy@users.noreply.github.com>
2024-04-09 13:00:19 -0600

    Lab 6 V2 Complete.
    Meets functional requirements and complete with comment headers for all functions (omit main).
    
    Can anyone reading this try this program on their machine? I wonder if I may have messed something up for the animations to be slower on my end.

--- Lab 6/lab6V2_nedillon.py
+++ Lab 6/lab6V2_nedillon.py
@@ -9,9 +9,9 @@
 # Electrical and Computer Engineering
 # All rights reserved.
 #
-# Student name: Nathan Edillon 95%
+# Student name: Nathan Edillon 92%
 # Student CCID: nedillon
-# Others: GOLDEN 2%, TOPGUN 3%
+# Others: GOLDEN 5%, TOPGUN 3%
 #
 # To avoid plagiarism, list the names of persons, Version 0 author(s)
 # excluded, whose code, words, ideas, or data you used. To avoid
@@ -35,6 +35,10 @@ def main():
     (images,errors) = gerchbergSaxton(im,10,Dphi,mask)
     saveFrames(images,errors)
 
+## Load Image
+#  @param name - name of image
+#  @return im - pixel data of image
+#  reads an image file and converts into raw pixel data
 def loadImage(name):
     im = plt.imread(name)/255
     if len(im.shape) > 2:
@@ -43,8 +47,16 @@ def loadImage(name):
     im[im > 1] = 1
     return im
 
-def opticalSystem(im,width):
-    (im, mask) = occultCircle(im,width)
+## Optical System
+#  @param im - image data
+#  @param diameter - desired diameter of circle
+#  @return im - altered image data
+#  @return Dphi - phase aberration in the pupil plane of the coronagraph
+#  @return mask - pixels masked by the circle
+#  places a circle in the original photo data and performs some Fourier
+#  transformation
+def opticalSystem(im,diameter):
+    (im, mask) = occultCircle(im,diameter)
     (IMa,IMp) = dft2(im)
     rng = np.random.default_rng(12345)
     imR = rng.random(im.shape)
@@ -54,17 +66,18 @@ def opticalSystem(im,width):
 
 ## Occult Circle
 #  @param im - image data
-#  @param width - desired diameter of circle
+#  @param diameter - desired diameter of circle
 #  @return im - altered image data
 #  @return mask - data of pixels altered with the black circle (boolean)
-#  take an image and place a circle of desired diameter in the center
-def occultCircle(im,width):
-    # im[im.shape[0]//2 - width//2 : im.shape[0]//2 + width//2,
-    #     im.shape[1]//2 - width//2 : im.shape[1]//2 + width//2] = 0
+#  take an image and place a black circle of desired diameter in the center
+def occultCircle(im,diameter):
     mask = np.full(im.shape, False, dtype = bool)
-    for x in range(im.shape[0]//2 - width//2, im.shape[0]//2 + width//2):    
-        for y in range(im.shape[1]//2 - width//2, im.shape[1]//2 + width//2):
-            if np.sqrt((x - im.shape[0]//2)**2 + (y - im.shape[1]//2)**2) <= width//2:
+    radius = diameter // 2
+    xCenter = im.shape[0] // 2
+    yCenter = im.shape[1] // 2
+    for x in range(xCenter - radius, xCenter + radius):    
+        for y in range(yCenter - radius, yCenter + radius):
+            if np.sqrt((x - xCenter)**2 + (y - yCenter)**2) <= radius:
                 im[x, y] = 0
                 mask[x, y] = True
     return (im, mask)
@@ -88,13 +101,24 @@ def idft2(IMa,IMp):
     im[im > 1] = 1
     return im
 
+## Occult Error
+#  @param im - image data
+#  @param mask - masked pixel data
+#  @return error - sum of squared data of masked pixels
+#  the error is the sum of all squared values of masked pixels
+def occultError(im,mask):
+    error = np.sum(im[mask]**2)
+    return error
+
 ## Gerchberg Saxton Algorithm
 #  @param im - image data
 #  @param maxIters - the amount of iterations to create
 #  @param Dphi - phase aberration in the pupil plane of the coronagraph
-#  @return - images - image data post-algorithm
-#  does some math to simulate the GS algorithm, creating a set of images
-#  of a desired amount of iterations
+#  @param mask - masked pixel data
+#  @return images - image data post-algorithm
+#  @return errors - data of mask error
+#  does some math to simulate the GS algorithm, creating a set of images of
+#  a desired amount of iterations, printing its progression in the console
 def gerchbergSaxton(im,maxIters,Dphi,mask):
     (IMa,IMp) = dft2(im)
     images = []
@@ -103,30 +127,36 @@ def gerchbergSaxton(im,maxIters,Dphi,mask):
         print("Iteration %d of %d" % (k,maxIters))
         im = idft2(IMa,IMp+(k/maxIters*Dphi))
         images.append(im)
-        error = 0
-        error += im[mask]**2
-        errors.append(error)
-                    
+        errors.append(occultError(im,mask))       
     return (images,errors)
 
 ## Save Frames
 #  @param images - data of a set of images
-#  takes data that forms images to form black and white photos, shows each
-#  iteration of the photos in succession, and saves them as "coronograph"
-#  .png files
+#  @param errors - masked error data
+#  takes data that forms black and white images to simulate a coronagraph, 
+#  graphs error data per iteration on top of the simulation and saves them 
+#  as "coronograph" .png files
 def saveFrames(images,errors):
     shape = (images[0].shape[0],images[0].shape[1],3)
     image = np.zeros(shape,images[0].dtype)
     maxIters = len(images)-1
+    iterations = []
+    sumError = []
+    maxError = max(errors)
     for k in range(maxIters+1):
         image[:,:,0] = images[k]
         image[:,:,1] = images[k]
         image[:,:,2] = images[k]
-        plt.subplot(1, 2, 1)
-        plt.subplot(1, 2, 2)
-        plt.imshow(image)
-        plt.axis('off')
-        plt.title("Iteration %i of %i" % (k, maxIters))
+        iterations.append(k)
+        sumError.append(errors[k])
+        plt.xlabel("Iteration")
+        plt.ylabel("Sum Square Error")
+        plt.xlim(0, maxIters)
+        plt.ylim(0, maxError)
+        plt.plot(iterations, sumError, "r-")
+        plt.imshow(image, extent = (0, maxIters, 0, maxError))
+        plt.gca().set_aspect(maxIters/maxError)
+        plt.title("Coronagraph Simulation")
         plt.savefig('coronagraph'+str(k)+'.png')
         plt.show()
 
